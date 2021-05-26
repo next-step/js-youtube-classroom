527bb76817033c2dd504b4dda6ec1ddc304fda23
JunilHwang <junil.h@kakao.com>
2021-05-26 23:25:17 +0900

    feat: RestClient 작업 완료

---
+++ step1/src/_core/RestClient.ts
@@ -0,0 +1,44 @@
+enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+export class RestClient {
+
+  constructor(
+    private readonly baseUrl: string
+  ) {}
+
+  private request<T> (method: HttpMethod, uri: string): Promise<T> {
+    const url = new URL(uri, this.baseUrl);
+    return fetch(url.href, { method }).then(response => response.json());
+  }
+
+  private requestWithBody<T, B> (method: HttpMethod, uri: string, body: B): Promise<T> {
+    const url = new URL(uri, this.baseUrl);
+    return fetch(url.href, { method, body: JSON.stringify(body) }).then(response => response.json());
+  }
+
+  public get<T> (uri: string): Promise<T> {
+    return this.request<T>(HttpMethod.GET, uri);
+  }
+
+  public post<T, B> (uri: string, body: B): Promise<T> {
+    return this.requestWithBody<T, B>(HttpMethod.POST, uri, body);
+  }
+
+  public patch<T, B> (uri: string, body: B): Promise<T> {
+    return this.requestWithBody<T, B>(HttpMethod.PATCH, uri, body);
+  }
+
+  public put<T, B> (uri: string, body: B): Promise<T> {
+    return this.requestWithBody<T, B>(HttpMethod.PUT, uri, body);
+  }
+
+  public delete<T> (uri: string): Promise<T> {
+    return this.request(HttpMethod.DELETE, uri);
+  }
+}
