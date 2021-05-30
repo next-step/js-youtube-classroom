db34c6aad405835fecb0ecf69c58cc506a505130
HaJunRyu <hjr4557@naver.com>
2021-05-31 03:50:39 +0900

    Feat: youtube 검색과 무한스크롤 구현을 위한 api 요청함수 생성
    new file:   src/js/api/ajax.js

---
+++ src/js/api/ajax.js
@@ -0,0 +1,19 @@
+const { YOUTUBE_BASE_URL } = process.env;
+const { YOUTUBE_API_KEY } = process.env;
+
+const MAX_RESULTS = 1;
+
+const ajax = {
+  searchYoutubeByTitle(q) {
+    return fetch(
+      `${YOUTUBE_BASE_URL}/search?q=${q}&part=snippet&key=${YOUTUBE_API_KEY}&maxResults=${MAX_RESULTS}`
+    );
+  },
+  nextPage({ q, nextPageToken }) {
+    return fetch(
+      `${YOUTUBE_BASE_URL}/search?q=${q}&part=snippet&key=${YOUTUBE_API_KEY}&maxResults=${MAX_RESULTS}&pageToken=${nextPageToken}`
+    );
+  },
+};
+
+export default ajax;
