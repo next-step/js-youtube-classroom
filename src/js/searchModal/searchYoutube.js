f9dcd10b8aa1e4fa666b11a4a9e1b2af616ec4dd
bcround <sbc1142@gmail.com>
2021-05-31 18:03:13 +0900

    Feat: 무한 스크롤 구현 (10개씩 불러와짐) modified:   src/js/searchModal/searchYoutube.js

--- src/js/searchModal/searchYoutube.js
+++ src/js/searchModal/searchYoutube.js
@@ -1,24 +1,47 @@
 import api from 'apis/api';
-import showSkeleton from 'utils/skeleton';
-import { renderYoutubeItems, renderNotFoundMessage } from 'utils/render';
+import {
+  renderYoutubeItems,
+  renderNotFoundMessage,
+  renderSkeleton,
+} from 'utils/render';
 
 const $modalVideoWrapper = document.querySelector('.modal .video-wrapper');
 const $searchInput = document.querySelector('.modal form .w-100');
 const $searchButton = document.querySelector('.modal form .btn');
+const $modalInner = document.querySelector('.modal-inner');
+const $div = document.createElement('div');
+$div.setAttribute('id', 'modalFetchMore');
+
+const searchInfo = { nextPage: '', value: '' };
 
 const search = async () => {
   const { value } = $searchInput;
+  searchInfo.value = value;
 
-  showSkeleton($modalVideoWrapper, 10);
+  renderSkeleton($modalVideoWrapper, 10);
 
   const response = await api.searchYoutube(value);
   const {
-    data: { items },
+    data: { items, nextPageToken },
   } = response;
+  const isNotFound = !items.length;
+  searchInfo.nextPage = nextPageToken;
 
-  items.length
-    ? renderYoutubeItems($modalVideoWrapper, items)
-    : renderNotFoundMessage($modalVideoWrapper);
+  isNotFound
+    ? renderNotFoundMessage($modalVideoWrapper)
+    : renderYoutubeItems($modalVideoWrapper, items);
+
+  $modalInner.appendChild($div);
+};
+
+const searchNextPage = async (value, pageToken) => {
+  const response = await api.searchNextPageYoutube(value, pageToken);
+  const {
+    data: { items, nextPageToken },
+  } = response;
+  searchInfo.nextPage = nextPageToken;
+
+  renderYoutubeItems($modalVideoWrapper, items);
 };
 
 const onEnterSearch = e => {
@@ -32,5 +55,11 @@ const onClickSearch = () => {
   search();
 };
 
-$searchInput.addEventListener('keydown', onEnterSearch);
+const fetchMoreObserver = new IntersectionObserver(([{ isIntersecting }]) => {
+  if (isIntersecting) searchNextPage(searchInfo.value, searchInfo.nextPage);
+});
+
+fetchMoreObserver.observe($div);
+
+$searchInput.addEventListener('keypress', onEnterSearch);
 $searchButton.addEventListener('click', onClickSearch);
