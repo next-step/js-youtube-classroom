e44d4e0111cdcdcfc38ba0c45b6b62ed6b8593f1
junilhwang <tjsdlf4261@naver.com>
2021-06-01 17:36:47 +0900

    refactor: Store type 리팩토링

--- step1/src/_core/Store.ts
+++ step1/src/_core/Store.ts
@@ -1,22 +1,37 @@
 import {observable} from "~_core/Observer";
 
+type Commit<State> = (state: State, payload: any) => void;
+type Dispatch<State> = (context: Context<State>, ...params: any[]) => any
+
+interface Context<State> {
+  state: State;
+  commit: (type: string, payload: any) => void;
+  dispatch: (type: string, ...params: any[]) => any;
+}
+
+interface StoreProps<State> {
+  state: State;
+  mutations: Record<string, Commit<State>>;
+  actions: Record<string, Dispatch<State>>
+}
+
 export class Store<State> {
 
   public $state: State;
-  private readonly mutations: Record<string, Function>;
-  private readonly actions: Record<string, Function>;
+  private readonly mutations: Record<string, Commit<State>>;
+  private readonly actions: Record<string, Dispatch<State>>;
 
-  constructor({ state, mutations, actions }: { state: State, mutations: Record<string, Function>, actions: Record<string, Function> }) {
+  constructor({ state, mutations, actions }: StoreProps<State>) {
     this.$state = observable(state);
     this.mutations = mutations;
     this.actions = actions;
   }
 
-  public commit(type: string, ...params: unknown[]): void {
-    this.mutations[type](this.$state, ...params);
+  public commit(type: string, payload: any): void {
+    this.mutations[type](this.$state, payload);
   }
 
-  public dispatch(type: string, ...params: unknown[]): unknown {
+  public dispatch(type: string, ...params: any[]): any {
     return this.actions[type]({
       state: Object.freeze(this.$state),
       commit: this.commit.bind(this),
