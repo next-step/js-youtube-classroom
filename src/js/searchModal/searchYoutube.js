7c66013a1fc92d61ac4f3cc3bbae25542cd08baa
bcround <sbc1142@gmail.com>
2021-06-01 10:52:06 +0900

    Feat: 검색한 영상을 저장할 수 있는 기능 추가 modified:   src/js/searchModal/searchYoutube.js

--- src/js/searchModal/searchYoutube.js
+++ src/js/searchModal/searchYoutube.js
@@ -3,6 +3,8 @@ import {
   renderYoutubeItems,
   renderNotFoundMessage,
   renderSkeleton,
+  hideSkeleton,
+  refreshItems,
 } from 'utils/render';
 
 const $modalVideoWrapper = document.querySelector('.modal .video-wrapper');
@@ -12,10 +14,16 @@ const $modalInner = document.querySelector('.modal-inner');
 const $div = document.createElement('div');
 $div.setAttribute('id', 'modalFetchMore');
 
-const searchInfo = { nextPage: '', value: '' };
+const searchInfo = { nextPage: '', value: '', items: [] };
+const localSavedItems = JSON.parse(localStorage.getItem('savedItems'));
+let savedItems = [];
+let isSaved = false;
 
 const search = async () => {
   const { value } = $searchInput;
+
+  if (searchInfo.value !== value) refreshItems($modalVideoWrapper);
+
   searchInfo.value = value;
 
   renderSkeleton($modalVideoWrapper, 10);
@@ -25,23 +33,68 @@ const search = async () => {
     data: { items, nextPageToken },
   } = response;
   const isNotFound = !items.length;
+  searchInfo.items = items;
   searchInfo.nextPage = nextPageToken;
 
-  isNotFound
-    ? renderNotFoundMessage($modalVideoWrapper)
-    : renderYoutubeItems($modalVideoWrapper, items);
+  hideSkeleton($modalVideoWrapper);
+
+  if (isNotFound) renderNotFoundMessage($modalVideoWrapper);
+  else if (localSavedItems)
+    items.forEach(item => {
+      const {
+        id: { videoId },
+      } = item;
+      localSavedItems.forEach(localSavedItem => {
+        const {
+          id: { videoId: localVideoId },
+        } = localSavedItem;
+
+        if (videoId === localVideoId) isSaved = true;
+      });
+      renderYoutubeItems($modalVideoWrapper, item, isSaved);
+      isSaved = false;
+    });
+  else {
+    items.forEach(item =>
+      renderYoutubeItems($modalVideoWrapper, item, isSaved)
+    );
+  }
 
   $modalInner.appendChild($div);
 };
 
 const searchNextPage = async (value, pageToken) => {
+  renderSkeleton($modalVideoWrapper, 10);
+
   const response = await api.searchNextPageYoutube(value, pageToken);
   const {
     data: { items, nextPageToken },
   } = response;
   searchInfo.nextPage = nextPageToken;
+  searchInfo.items = [...searchInfo.items, ...items];
 
-  renderYoutubeItems($modalVideoWrapper, items);
+  hideSkeleton($modalVideoWrapper);
+
+  if (localSavedItems)
+    items.forEach(item => {
+      const {
+        id: { videoId },
+      } = item;
+      localSavedItems.forEach(localSavedItem => {
+        const {
+          id: { videoId: localVideoId },
+        } = localSavedItem;
+
+        if (videoId === localVideoId) isSaved = true;
+      });
+      renderYoutubeItems($modalVideoWrapper, item, isSaved);
+      isSaved = false;
+    });
+  else {
+    items.forEach(item =>
+      renderYoutubeItems($modalVideoWrapper, item, isSaved)
+    );
+  }
 };
 
 const onEnterSearch = e => {
@@ -55,6 +108,26 @@ const onClickSearch = () => {
   search();
 };
 
+const onClickSave = e => {
+  if (!e.target.matches('.video-wrapper button')) return;
+
+  const $clip = e.target.closest('.clip');
+  const $iframe = $clip.querySelector('iframe');
+  const iframeUrlSplit = $iframe.getAttribute('src').split('/');
+  const clickedVideoId = iframeUrlSplit[iframeUrlSplit.length - 1];
+  const clickedVideo = searchInfo.items.find(
+    ({ id: { videoId } }) => videoId === clickedVideoId
+  );
+
+  if (localStorage.getItem('savedItems')) {
+    savedItems = JSON.parse(localStorage.getItem('savedItems'));
+  }
+
+  savedItems = [...savedItems, clickedVideo];
+  localStorage.setItem('savedItems', JSON.stringify(savedItems));
+  e.target.setAttribute('disabled', true);
+};
+
 const fetchMoreObserver = new IntersectionObserver(([{ isIntersecting }]) => {
   if (isIntersecting) searchNextPage(searchInfo.value, searchInfo.nextPage);
 });
@@ -63,3 +136,4 @@ fetchMoreObserver.observe($div);
 
 $searchInput.addEventListener('keypress', onEnterSearch);
 $searchButton.addEventListener('click', onClickSearch);
+$modalVideoWrapper.addEventListener('click', onClickSave);
