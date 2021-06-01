30a1ffebc034c4af39d5d433e9cc101233916a5a
bcround <sbc1142@gmail.com>
2021-06-01 12:13:28 +0900

    Feat: 최대 저장 갯수를 100개로 한정함 modified:   src/js/searchModal/searchYoutube.js

--- src/js/searchModal/searchYoutube.js
+++ src/js/searchModal/searchYoutube.js
@@ -11,69 +11,20 @@ const $modalVideoWrapper = document.querySelector('.modal .video-wrapper');
 const $searchInput = document.querySelector('.modal form .w-100');
 const $searchButton = document.querySelector('.modal form .btn');
 const $modalInner = document.querySelector('.modal-inner');
+const $savedCount = document.querySelector('.saved-count');
 const $div = document.createElement('div');
 $div.setAttribute('id', 'modalFetchMore');
 
 const searchInfo = { nextPage: '', value: '', items: [] };
-const localSavedItems = JSON.parse(localStorage.getItem('savedItems'));
+const initialSavedItems = JSON.parse(localStorage.getItem('savedItems'));
 let savedItems = [];
 let isSaved = false;
+let savedCount = initialSavedItems ? initialSavedItems.length : 0;
 
-const search = async () => {
-  const { value } = $searchInput;
+$savedCount.innerText = initialSavedItems ? initialSavedItems.length : '0';
 
-  if (searchInfo.value !== value) refreshItems($modalVideoWrapper);
-
-  searchInfo.value = value;
-
-  renderSkeleton($modalVideoWrapper, 10);
-
-  const response = await api.searchYoutube(value);
-  const {
-    data: { items, nextPageToken },
-  } = response;
-  const isNotFound = !items.length;
-  searchInfo.items = items;
-  searchInfo.nextPage = nextPageToken;
-
-  hideSkeleton($modalVideoWrapper);
-
-  if (isNotFound) renderNotFoundMessage($modalVideoWrapper);
-  else if (localSavedItems)
-    items.forEach(item => {
-      const {
-        id: { videoId },
-      } = item;
-      localSavedItems.forEach(localSavedItem => {
-        const {
-          id: { videoId: localVideoId },
-        } = localSavedItem;
-
-        if (videoId === localVideoId) isSaved = true;
-      });
-      renderYoutubeItems($modalVideoWrapper, item, isSaved);
-      isSaved = false;
-    });
-  else {
-    items.forEach(item =>
-      renderYoutubeItems($modalVideoWrapper, item, isSaved)
-    );
-  }
-
-  $modalInner.appendChild($div);
-};
-
-const searchNextPage = async (value, pageToken) => {
-  renderSkeleton($modalVideoWrapper, 10);
-
-  const response = await api.searchNextPageYoutube(value, pageToken);
-  const {
-    data: { items, nextPageToken },
-  } = response;
-  searchInfo.nextPage = nextPageToken;
-  searchInfo.items = [...searchInfo.items, ...items];
-
-  hideSkeleton($modalVideoWrapper);
+const checkWhetherSaved = items => {
+  const localSavedItems = JSON.parse(localStorage.getItem('savedItems'));
 
   if (localSavedItems)
     items.forEach(item => {
@@ -97,6 +48,46 @@ const searchNextPage = async (value, pageToken) => {
   }
 };
 
+const search = async () => {
+  const { value } = $searchInput;
+
+  if (searchInfo.value !== value) refreshItems($modalVideoWrapper);
+
+  searchInfo.value = value;
+
+  renderSkeleton($modalVideoWrapper, 10);
+
+  const response = await api.searchYoutube(value);
+  const {
+    data: { items, nextPageToken },
+  } = response;
+  const isNotFound = !items.length;
+  searchInfo.items = items;
+  searchInfo.nextPage = nextPageToken;
+
+  hideSkeleton($modalVideoWrapper);
+
+  isNotFound
+    ? renderNotFoundMessage($modalVideoWrapper)
+    : checkWhetherSaved(items);
+
+  $modalInner.appendChild($div);
+};
+
+const searchNextPage = async (value, pageToken) => {
+  renderSkeleton($modalVideoWrapper, 10);
+
+  const response = await api.searchNextPageYoutube(value, pageToken);
+  const {
+    data: { items, nextPageToken },
+  } = response;
+  searchInfo.nextPage = nextPageToken;
+  searchInfo.items = [...searchInfo.items, ...items];
+
+  hideSkeleton($modalVideoWrapper);
+  checkWhetherSaved(items);
+};
+
 const onEnterSearch = e => {
   if (e.key === 'Enter') {
     e.preventDefault();
@@ -109,6 +100,9 @@ const onClickSearch = () => {
 };
 
 const onClickSave = e => {
+  const localSavedItems = JSON.parse(localStorage.getItem('savedItems'));
+
+  if (localSavedItems && localSavedItems.length >= 100) return;
   if (!e.target.matches('.video-wrapper button')) return;
 
   const $clip = e.target.closest('.clip');
@@ -122,9 +116,12 @@ const onClickSave = e => {
   if (localStorage.getItem('savedItems')) {
     savedItems = JSON.parse(localStorage.getItem('savedItems'));
   }
-
   savedItems = [...savedItems, clickedVideo];
+
   localStorage.setItem('savedItems', JSON.stringify(savedItems));
+  savedCount++;
+
+  $savedCount.innerText = savedCount;
   e.target.setAttribute('disabled', true);
 };
 
