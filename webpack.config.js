d8a53ce8dae9d2bc939a08c476d754d7360385a3
moonee <hellomooneekim@gmail.com>
2021-06-02 01:33:19 +0900

    chroe : 웹팩 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-const dotenv = require("dotenv").config({
-  path: path.join(__dirname, ".env"),
-});
+const dotenv = require("dotenv").config();
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
