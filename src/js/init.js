57892be8977dda13db30f752d772c1241dfce29e
bcround <sbc1142@gmail.com>
2021-06-02 13:48:33 +0900

    Feat: 볼 영상들 처음 웹페이지 켰을때 렌더링 되는 로직 추가 modified:   src/js/init.js

--- src/js/init.js
+++ src/js/init.js
@@ -1,11 +1,12 @@
 import { renderChips, renderNoResult, renderYoutubeItems } from 'utils/render';
 import { iterate } from 'utils/iterate';
 
-const $mainVideoWrapper = document.querySelector('main .video-wrapper');
-
-const savedVideos = JSON.parse(localStorage.getItem('savedItems'));
-
+// 최근 검색어 보여주기
 renderChips(JSON.parse(localStorage.getItem('lastestSearches')));
+
+// 볼영상들 처음 화면에 들어왔을때 보여주기
+const $mainVideoWrapper = document.querySelector('main .video-wrapper');
+const savedVideos = JSON.parse(localStorage.getItem('savedItems'));
 
 savedVideos
   ? iterate(renderYoutubeItems, savedVideos, {
