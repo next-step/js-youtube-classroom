fdc452e5cebe3823c54fdbe0f3faf2f95e4d7616
jinn2u <bsybear623@gmail.com>
2021-07-08 22:46:45 +0900

    [modal-searchBtn] 검색버튼 클릭시 youtube API 호출

--- src/js/View/ModalView.js
+++ src/js/View/ModalView.js
@@ -6,17 +6,20 @@ const tag = "[ModalView]"
 export default class ModalView extends View{
   constructor(){
     console.log(tag, 'View')
+
     super(qs(".modal"))
     this.inputElement = qs("[type=text]", this.element)
+    this.searchBtnElement = qs("[type=button]", this.element)
     this.bindEvent()
   }
   bindEvent() {
     on(this.element,"submit", (event) =>this.handleSubmit(event))
+    on(this.searchBtnElement, "click", (event)=>this.handleSubmit(event))
   }
   handleSubmit(event){
     event.preventDefault()
     const {value} = this.inputElement
     // console.log(tag, "handleSubmit", value)
     this.emit("@submit", {value}) 
-  } 
+  }
 }
