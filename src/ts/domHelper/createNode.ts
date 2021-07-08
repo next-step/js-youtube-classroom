cfc1d43ae6f2175e526794398f272807b0a9f336
HaJunRyu <hjr4557@naver.com>
2021-07-09 03:05:53 +0900

    Feat: ceateNode함수 사용법 변경
    children의 타입이 Node[]였는데 이렇게 하면 사용할때 배열내부에서 컴포넌트 함수를 호출해야하는 일이 발생하여
    createNode 함수내부에서 children요소들을 호출할 수 있게끔 변경
    
    modified:   src/ts/domHelper/createNode.ts

--- src/ts/domHelper/createNode.ts
+++ src/ts/domHelper/createNode.ts
@@ -1,9 +1,16 @@
-const createNode = (DOMstring: string, children: Node[]) => {
+import { Component } from '../types';
+
+const createNode = (DOMstring: string, children?: Component[]) => {
   const template = document.createElement('template');
   template.innerHTML = DOMstring;
 
   const node = template.content.firstElementChild.cloneNode(true);
-  children.forEach(child => node.appendChild(child));
+
+  if (children) {
+    children.forEach(child => {
+      node.appendChild(child());
+    });
+  }
 
   return node;
 };
