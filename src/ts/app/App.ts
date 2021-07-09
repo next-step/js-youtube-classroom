ad90eeb7a330fa3a6da81d1f6b207813ac5bee21
HaJunRyu <hjr4557@naver.com>
2021-07-10 08:08:58 +0900

    Feat: App컴포넌트에 Main영역과 Modal영역 렌더링
    Changes to be committed:
    modified:   src/ts/app/App.ts

--- src/ts/app/App.ts
+++ src/ts/app/App.ts
@@ -1,9 +1,12 @@
+import { Modal } from '../components';
 import { createNode } from '../domHelper';
 import { Home } from '../pages';
 import { Component } from '../types';
 
-const App: Component = () => {
-  const $app = createNode(`<div id="app"></div>`, [Home]);
+interface Props {}
+
+const App: Component<Props> = () => {
+  const $app = createNode(`<div id="app"></div>`, [Home({}), Modal({})]);
 
   return $app;
 };
