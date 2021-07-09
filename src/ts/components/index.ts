56d6f2390ac4a733eec8a1bf6d381217516918a8
HaJunRyu <hjr4557@naver.com>
2021-07-10 08:08:22 +0900

    Feat: 현재까지 추가한 컴포넌트들을 index.ts파일에 모듈화
    Changes to be committed:
    modified:   src/ts/components/index.ts

--- src/ts/components/index.ts
+++ src/ts/components/index.ts
@@ -1,1 +1,8 @@
 export { default as Modal } from './Modal';
+export { default as Header } from './Header';
+export { default as Main } from './Main';
+export { default as SaveVideoSection } from './SaveVideoSection';
+export { default as Video } from './Video';
+export { default as Button } from './Button';
+export { default as Navigator } from './Navigator';
+export { default as Heading } from './Heading';
