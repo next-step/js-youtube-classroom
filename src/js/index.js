890ddcb4c385a5bc8d3d5aa51b5feb26a574517b
inhyuck <dlsgur1447@gmail.com>
2021-07-11 19:04:23 +0900

    🎉 chore: 포매팅 및 .gitignore 설정

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-const $searchButton = document.querySelector("#search-button");
-const $modalClose = document.querySelector(".modal-close");
-const $modal = document.querySelector(".modal");
+const $searchButton = document.querySelector('#search-button');
+const $modalClose = document.querySelector('.modal-close');
+const $modal = document.querySelector('.modal');
 
 const onModalShow = () => {
-  $modal.classList.add("open");
+    $modal.classList.add('open');
 };
 
 const onModalClose = () => {
-  $modal.classList.remove("open");
+    $modal.classList.remove('open');
 };
 
-$searchButton.addEventListener("click", onModalShow);
-$modalClose.addEventListener("click", onModalClose);
+$searchButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
