e365055a67e7ae9c49859abcfb83ae9733094a5b
suyeon woo <newnorm@olulo.io>
2021-07-14 21:51:11 +0900

    [2기 newnorm] 🚀 나만의 유튜브 강의실 미션 1단계 (#9)
    * gitignore 에 idea 추가
    
    * 리드미 추가
    
    * dotenv, http-server 설치 / 리드미 구현계획 수정
    
    * 유튜브 data api 테스트 fetch

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
+// import dotenv from 'dotenv';
+// dotenv.config()
+// const apiKey = process.env.YOUTUBE_DATA_API_KEY
+let apiKey = ''
+// TODO: dotenv 설치하기 ?
+
+
 const $searchButton = document.querySelector("#search-button");
 const $modalClose = document.querySelector(".modal-close");
 const $modal = document.querySelector(".modal");
+const $startSearchButton = document.querySelector("#start-search-button");
 
 const onModalShow = () => {
   $modal.classList.add("open");
@@ -10,5 +18,14 @@ const onModalClose = () => {
   $modal.classList.remove("open");
 };
 
+const startSearch = async () => {
+  // TODO:env 에서 apiKey 불러오기
+  const url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${apiKey}`
+  const response = await fetch(url)
+  console.log("response", response)  // response has came
+
+}
+
 $searchButton.addEventListener("click", onModalShow);
 $modalClose.addEventListener("click", onModalClose);
+$startSearchButton.addEventListener("click", startSearch)
