5e0dfddc47aad2b97a8003d377ade0a8ca3db1fb
HaJunRyu <hjr4557@naver.com>
2021-07-17 15:54:42 +0900

    Modify: Youtube API search 요청시 video데이터만 검색되게끔 파라미터 추가
    Changes to be committed:
    modified:   src/ts/api/youtube.ts

--- src/ts/api/youtube.ts
+++ src/ts/api/youtube.ts
@@ -5,14 +5,14 @@ const MAX_RESULTS = 10;
 
 const searchYoutubeByTitle = async (searchKeyword: string) => {
   const res = await customAxios.get(
-    `/search?key=${YOUTUBE_API_KEY}&part=snippet&maxResults=${MAX_RESULTS}&q=${searchKeyword}`
+    `/search?key=${YOUTUBE_API_KEY}&part=snippet&maxResults=${MAX_RESULTS}&q=${searchKeyword}&type=video`
   );
   return await res.data;
 };
 
 const nextPage = async (searchKeyword: string, nextPageToken: string) => {
   const res = await customAxios.get(
-    `/search?key=${YOUTUBE_API_KEY}&part=snippet&maxResults=${MAX_RESULTS}&pageToken=${nextPageToken}&q=${searchKeyword}`
+    `/search?key=${YOUTUBE_API_KEY}&part=snippet&maxResults=${MAX_RESULTS}&pageToken=${nextPageToken}&q=${searchKeyword}&type=video`
   );
   return await res.data;
 };
