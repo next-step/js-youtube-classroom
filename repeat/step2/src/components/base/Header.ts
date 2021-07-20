bd26e41c25e6fe2f2e12c1d22774e9fec0244dea
JunilHwang <junil.h@kakao.com>
2021-07-20 23:54:50 +0900

    feat: router 작업 완료

--- repeat/step2/src/components/base/Header.ts
+++ repeat/step2/src/components/base/Header.ts
@@ -1,4 +1,5 @@
 import {addEvent} from "~@core";
+import {router} from "~router";
 
 export interface HeaderProps {
   openModal: () => void;
@@ -14,11 +15,19 @@ export const Header = ({ openModal }: HeaderProps) => {
     <header class="my-4">
       <h2 class="text-center font-bold">👩🏻‍💻 나만의 유튜브 강의실 👨🏻‍💻</h2>
       <nav class="d-flex justify-center">
-        <button class="btn bg-cyan-100 mx-1">👁️ 볼 영상</button>
-        <button class="btn mx-1">✅ 본 영상</button>
+      
+        <a href="/#!" class="btn ${router.path === '/' ? 'bg-cyan-100' : ''} mx-1">
+          👁️ 볼 영상
+        </a>
+        
+        <a href="/#!/viewed" class="btn ${router.path === '/viewed' ? 'bg-cyan-100' : ''} mx-1">
+          ✅ 본 영상
+        </a>
+        
         <button id="search-button" class="btn mx-1">
           🔍 동영상 검색
         </button>
+        
       </nav>
     </header>
   `;
