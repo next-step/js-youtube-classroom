1268c84dff9221a29ebc205b2bb37bf28f91741f
Yeewon <yes3427@kookmin.ac.kr>
2021-07-21 16:40:21 +0900

    feat: snackbar 메시지 추가

---
+++ src/js/constants/snackbar.js
@@ -0,0 +1,7 @@
+export const SAVE_SUCCESS_MSG = `저장된 동영상에 추가됨`;
+export const SAVE_CANCEL_SUCCESS_MSG = `저장된 동영상에서 삭제됨`;
+export const DELETE_SUCCESS_MSG = `동영상이 삭제됨`;
+export const TO_WATCH_SUCCESS_MSG = `볼 동영상에 추가됨`;
+export const WATCHED_SUCCESS_MSG = `본 동영상에 추가됨`;
+export const LIKED_SUCCESS_MSG = `좋아요 표시한 동영상에 추가됨`;
+export const LIKE_CANCEL_SUCCESS_MSG = `좋아요 표시한 동영상에서 삭제됨`;
