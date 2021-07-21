9ea3efc9137af8ecc803380bad13c82ea8acd18f
inhyuck <dlsgur1447@gmail.com>
2021-07-21 18:44:01 +0900

    ⚰️ feat: 메인화면에 로컬스토리지에 저장된 동영상 조회

--- src/js/components/Articles.js
+++ src/js/components/Articles.js
@@ -1,46 +1,79 @@
+import {findAllByVideoIds} from '../apis/youtubeApis.js';
+import {getSavedVideos} from '../store/videoStore.js';
+
 export function Articles($el) {
 
+    const state = {
+        articles: [],
+    };
+
+    const setState = ({articles}) => {
+        state.articles = articles ?? state.articles;
+        render();
+    };
+
+    const articleNormalTemplate = ({videoId, title, channelId, channelTitle, publishedAt}) => `
+        <article class="clip">
+            <div class="preview-container">
+                <iframe
+                    width="100%"
+                    height="118"
+                    src="https://www.youtube.com/embed/${videoId}"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </div>
+            <div class="content-container pt-2 px-1">
+                <h3>${title}</h3>
+                <div>
+                    <a
+                        href="https://www.youtube.com/channel/${channelId}"
+                        target="_blank"
+                        class="channel-name mt-1"
+                    >
+                        ${channelTitle} 
+                    </a>
+                    <div class="meta">
+                        <p>${publishedAt}</p>
+                    </div>
+                    <div>
+                        <span class="opacity-hover">✅</span>
+                        <span class="opacity-hover">👍</span>
+                        <span class="opacity-hover">💬</span>
+                        <span class="opacity-hover">🗑️</span>
+                    </div>
+                </div>
+            </div>
+        </article>
+    `;
+
+    const loadArticles = async () => {
+        const videoIds = getSavedVideos();
+        const {items: articles} = await findAllByVideoIds({videoIds});
+        setState({articles});
+    };
+
     const render = () => {
+        const {articles} = state;
+        const articlesTemplate = articles.map(({
+                                                   videoId,
+                                                   channelId,
+                                                   channelTitle,
+                                                   title,
+                                                   publishedAt,
+                                               }) => articleNormalTemplate({videoId, title, channelId, channelTitle, publishedAt}))
+                                         .join('');
+
         $el.innerHTML = `
             <main class="mt-10">
                 <section class="video-wrapper">
-                    <article class="clip">
-                        <div class="preview-container">
-                            <iframe
-                                width="100%"
-                                height="118"
-                                src="https://www.youtube.com/embed/Ngj3498Tm_0"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                            ></iframe>
-                        </div>
-                        <div class="content-container pt-2 px-1">
-                            <h3>아두이노 무드등</h3>
-                            <div>
-                                <a
-                                    href="https://www.youtube.com/channel/UC-mOekGSesms0agFntnQang"
-                                    target="_blank"
-                                    class="channel-name mt-1"
-                                >
-                                    메이커준
-                                </a>
-                                <div class="meta">
-                                    <p>2021년 3월 2일</p>
-                                </div>
-                                <div>
-                                    <span class="opacity-hover">✅</span>
-                                    <span class="opacity-hover">👍</span>
-                                    <span class="opacity-hover">💬</span>
-                                    <span class="opacity-hover">🗑️</span>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
+                    ${articlesTemplate}     
                 </section>
             </main>
         `;
     };
 
     render();
+    loadArticles();
 }
